8088b10e1e236fdeab650085ac01b571ac261397
luismurao <luismurao@gmail.com>
2018-07-18 23:14:59 -0500

    doPparallel call for pROC

--- R/kuenm_proc.R
+++ R/kuenm_proc.R
@@ -73,8 +73,6 @@ kuenm_proc <- function(occ.test, model, threshold = 5, rand.percent = 50,
     cores <- parallel::detectCores()
     cl <- parallel::makeCluster(cores)
     doParallel::registerDoParallel(cl)
-    nCores <- foreach::getDoParWorkers()
-
     output_auc <- foreach::foreach(x = 1:(iterations),
                           .packages = c("kuenm","raster","dplyr")) %dopar%
 
